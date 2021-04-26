bf8066c6df1e30363d05d1e5e2e7a5596edb1432
hcarpe <hcarpe@users.noreply.github.com>
2021-04-25 23:02:38 -0400

    For Mundy to check

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
-d3.csv("/assets/csv/vis1ch1.csv", function(data) {
-    console.log(data);
-});
+var data = "assets/csv/viz1ch1.csv";
+var parsed = d3.csvParse(data);
+console.log(parsed);
+
+//d3.csv("/assets/csv/viz1ch1.csv", function(data) {
+//    console.log(data);
+//});
