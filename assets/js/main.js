949c63c184e3b471bb2879b716f575b89db8be33
hcarpe <hcarpe@users.noreply.github.com>
2021-04-26 22:42:44 -0400

    Adding data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-var data = "assets/csv/viz1ch1.csv";
-var parsed = d3.csvParse(data);
-console.log(parsed);
+//var data = "assets/csv/viz1ch1.csv";
+//var parsed = d3.csvParse(data);
+//console.log(parsed);
 
-//d3.csv("/assets/csv/viz1ch1.csv", function(data) {
-//    console.log(data);
-//});
+d3.csv("assets/csv/viz1ch1.csv", function(data) {
+  console.log(data);
+});
